bb300ace4049839dd3fb6cf86f43436dc2b50a45
David Cruz <juanchocruz200516@gmail.com>
2023-07-06 12:25:15 -0500

    Página Sobre Nosotros de la pastelería

---
+++ src/pages/SobreNosotros.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header"
+import styles from './SobreNosotros.module.css'
+
+function SobreNosotros() {
+  return (
+    <>
+      <Header />
+      <h1 className={styles.bienvenida}>Sobre nosotros</h1>
+      <div className={styles.info}>
+        <img src="https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/SobreNosotrosImg.webp" alt="Sobre nosotros img" />
+        <p>Somos una nueva pastelería con una meta clara, ofrecer el mejor servicio y calidad posible para nuestros
+          clientes con nuestros productos y servicios. <br />
+          Todos nuestros productos cuentan con la mejor garantía, si algo no le gusta con mucho gusto lo arreglamos.
+          <br />
+          Siéntase libre de adquirir alguno de nuestros pasteles, con toda la calidad que se merecen nuestros
+          clientes.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default SobreNosotros
