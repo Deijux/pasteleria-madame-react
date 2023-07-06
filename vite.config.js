44c179435c76b18562ed4fafb3d606596c21ee3a
David Cruz <juanchocruz200516@gmail.com>
2023-07-06 12:48:41 -0500

    base updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'https://deijux.github.io/pasteleria-madame-react/'
+  plugins: [react()]
 })
