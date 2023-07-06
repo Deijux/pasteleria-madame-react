1e4d759c1cb724ab761a519dea65319cc9eafc50
David Cruz <juanchocruz200516@gmail.com>
2023-07-06 12:26:04 -0500

    Se agrega las listas de los adornos y pasteles

---
+++ src/data/adornos.js
@@ -0,0 +1,35 @@
+const adornos = [
+  {
+    name: 'Caramelo',
+    price: 10,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Caramelo.webp',
+    id: 1
+  },
+  {
+    name: 'Frutas',
+    price: 15,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Frutas.webp',
+    id: 2
+  },
+  {
+    name: 'Fondat',
+    price: 20,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Fondat.webp',
+    id: 3
+  },
+  {
+    name: 'Chantilly',
+    price: 10,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Chantilly.webp',
+    id: 4
+  },
+  {
+    name: 'Merengue',
+    price: 10,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Merengue.webp',
+    id: 5
+  },
+  
+]
+
+export default adornos;
---
+++ src/data/pasteles.js
@@ -0,0 +1,34 @@
+const pastelList = [
+  {
+    name: 'Pastel de vainilla',
+    price: 30,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Pastel-vainilla.webp',
+    id: 1
+  },
+  {
+    name: 'Pastel de chocolate',
+    price: 30,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Pastel-chocolate.webp',
+    id: 2
+  },
+  {
+    name: 'Pastel de fresa',
+    price: 30,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Pastel-fresa.webp',
+    id: 3
+  },
+  {
+    name: 'Pastel de limón',
+    price: 30,
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/Pastel-lim%C3%B3n.webp',
+    id: 4
+  },
+  {
+    name: 'Más sabores próximamente',
+    img: 'https://raw.githubusercontent.com/Deijux/pasteleria-madame/main/img/coming-soon.webp',
+    price: 0,
+    id: 5
+  }
+]
+
+export default pastelList;
