41f22bc2eff44b10494638203b446bcae380d995
David Cruz <juanchocruz200516@gmail.com>
2023-07-06 12:18:50 -0500

    Se crearon las rutas con react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import SobreNosotros from './pages/SobreNosotros.jsx'
+import Login from './pages/Login.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/sobre-nosotros',
+    element: <SobreNosotros />,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+];
+
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
